9cf3937ba026cb5ef62d0b14daab7832895eb721
KurisuLim <kazuki1002003@yahoo.com>
2018-07-24 19:22:31 -0700

    sasa

--- server.js
+++ server.js
@@ -7,37 +7,49 @@ require('dotnev').config();
 
 
 const app = express();
-
 app.use(morgan('dev'))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(express.static(_dirname + '/public'));
 //Here we're setting the views diredtory to be ./views
 //thereby letting the app know where to find the template files
 app.set('views', './view');
-
-//Here we're setting the default engine to be ejs
-//note we don't need to require it, express will do that for us
 app.set('view engine', 'ejs');
 
-//Now instead of using res.send we can use
-//res.render to send the output of the template by filename
-app.get('/', (requestAnimationFrame,res) =>{
-    res.render('index');
+app.get('/', (req, res) => {
+  const data = {
+    person: {
+      firstName: 'Christopher',
+      lastName: 'Lim',
+    }
+  }
+// Notice now the data is the second argument passed to the template render method
+res.render('index', data);
 });
+
+app.get('/', (req,res) =>{
+    res.render('contact');
+});
+
+app.post('/thanks', (req,res)=> {
+  const msg = {
+    to: 'toffer.lim87@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  res.render("thanks", {contact: req.body})
+});
+
+app.get("*", function (req,res){
+  res.send("Opps. Something went wrong!...Err 404").status(404);
+})
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
 });
 
-app.get('/', (req, res) => {
-    const data = {
-      person: {
-        firstName: 'Chris',
-        lastName: 'Lim',
-      }
-    }
-  
-    // Notice now the data is the second argument passed to the template render method
-    res.render('index', data);
-  });
