283bfa0a4f3f6796ccc88a07eefad86932d32545
KurisuLim <kazuki1002003@yahoo.com>
2018-07-24 18:37:10 -0700

    asasaa

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'); 
+const morgan = require('morgan');
+const bodyParser = require ('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Here we're setting the views diredtory to be ./views
+//thereby letting the app know where to find the template files
+app.set('views', './view');
+
+//Here we're setting the default engine to be ejs
+//note we don't need to require it, express will do that for us
+app.set('view engine', 'ejs');
+
+//Now instead of using res.send we can use
+//res.render to send the output of the template by filename
+app.get('/', (requestAnimationFrame,res) =>{
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
+
+app.get('/', (req, res) => {
+    const data = {
+      person: {
+        firstName: 'Chris',
+        lastName: 'Lim',
+      }
+    }
+  
+    // Notice now the data is the second argument passed to the template render method
+    res.render('index', data);
+  });
